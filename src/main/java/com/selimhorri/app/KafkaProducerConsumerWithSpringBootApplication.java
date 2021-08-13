42f7f3456e9a48f068b42f33e5e4518d81db6cb8
SelimHorri <horri.slim@gmail.com>
2021-08-14 00:33:57 +0100

    init

---
+++ src/main/java/com/selimhorri/app/KafkaProducerConsumerWithSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.selimhorri.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerConsumerWithSpringBootApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerConsumerWithSpringBootApplication.class, args);
+	}
+	
+	
+	
+}
