9f10254188d665b289697b890a837377cef732f2
SelimHorri <horri.slim@gmail.com>
2021-08-15 22:41:42 +0100

    publish msg successfully into kafka topic

---
+++ src/main/java/com/selimhorri/app/config/KafkaConfig.java
@@ -0,0 +1,40 @@
+package com.selimhorri.app.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.selimhorri.app.model.Message;
+
+@Configuration
+public class KafkaConfig {
+	
+	private static final Object KAFKA_HOSTNAME = "localhost:9092";
+	
+	@Bean
+	public ProducerFactory<String, Message> getJsonProducerFactory() {
+		
+		final Map<String, Object> configs = new HashMap<>();
+		configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_HOSTNAME);
+		configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		
+		return new DefaultKafkaProducerFactory<>(configs);
+	}
+	
+	@Bean
+	public KafkaTemplate<String, Message> getKafkaTemplate() {
+		return new KafkaTemplate<>(this.getJsonProducerFactory());
+	}
+	
+	
+	
+}
