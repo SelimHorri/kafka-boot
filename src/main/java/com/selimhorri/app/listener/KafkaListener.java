14ddcd9c5b274d2d17313e1ac52b1cf372772ddf
SelimHorri <horri.slim@gmail.com>
2021-08-17 00:52:03 +0100

    map object from listener

--- src/main/java/com/selimhorri/app/listener/KafkaListener.java
+++ src/main/java/com/selimhorri/app/listener/KafkaListener.java
@@ -2,12 +2,26 @@ package com.selimhorri.app.listener;
 
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.selimhorri.app.model.Message;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class KafkaListener {
 	
+	private final ObjectMapper objectMapper;
+	
 	@org.springframework.kafka.annotation.KafkaListener(topics = {"NewTopic"}, groupId = "group_id")
-	public void consume(final String msg) {
-		System.out.println(msg);
+	public void consume(final String messageString) throws JsonMappingException, JsonProcessingException {
+		final Message message = this.objectMapper.readValue(messageString.toString(), Message.class);
+		System.err.println(message);
+		log.info("mapped object printed! {}", message);
 	}
 	
 	
