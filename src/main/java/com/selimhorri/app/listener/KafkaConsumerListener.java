6687cb1390bb0d2aa3c1e15dd06c8803a3df30cb
Selim Horri <horri.slim@gmail.com>
2021-10-07 21:45:23 +0100

    add explicit import after renaming the file

--- src/main/java/com/selimhorri/app/listener/KafkaConsumerListener.java
+++ src/main/java/com/selimhorri/app/listener/KafkaConsumerListener.java
@@ -1,6 +1,7 @@
 package com.selimhorri.app.listener;
 
 import org.springframework.stereotype.Component;
+import org.springframework.kafka.annotation.KafkaListener;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -17,7 +18,7 @@ public class KafkaConsumerListener {
 	
 	private final ObjectMapper objectMapper;
 	
-	@org.springframework.kafka.annotation.KafkaListener(topics = {"NewTopic"}, groupId = "group_id")
+	@KafkaListener(topics = {"NewTopic"}, groupId = "group_id")
 	public void consume(final String messageString) throws JsonMappingException, JsonProcessingException {
 		final Message message = this.objectMapper.readValue(messageString.toString(), Message.class);
 		System.err.println(message);
