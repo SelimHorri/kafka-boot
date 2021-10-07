abb2ee50f8e0033d24cc0517eea2c8b54b85608e
Selim Horri <horri.slim@gmail.com>
2021-10-07 21:42:36 +0100

    rename file

--- src/main/java/com/selimhorri/app/listener/KafkaConsumerListener.java
+++ src/main/java/com/selimhorri/app/listener/KafkaConsumerListener.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class KafkaListener {
+public class KafkaConsumerListener {
 	
 	private final ObjectMapper objectMapper;
 	
